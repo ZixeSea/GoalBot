fe4ceaa0953e2f45f895180ed50101230285435c
Rene Lindeboom <rene.turok13@gmail.com>
2022-02-19 12:08:11 +0100

    Added reload command to update slash commands

---
+++ src/slash/adminSlash/reload.js
@@ -0,0 +1,15 @@
+module.exports = {
+	run: async ({ GoalBot, interaction }) => {
+		const donatorRolesMember = interaction.member.roles.filter((r) => GoalBot.config.modRole.includes(r));
+		if (donatorRolesMember.length <= 0)
+			return interaction.createFollowup("You don't have to required role(s) to run the command.");
+
+		GoalBot.cmds.forEach((c) => {
+			GoalBot.Logger.console('Slash', `Edited ${c.slashJSON.name}, should now be up to date.`);
+			GoalBot.client.createGuildCommand(GoalBot.config.guild, c.slashJSON, 1);
+		});
+
+		return interaction.createFollowup('Reload from the slash commands is done.');
+	},
+	slashJSON: { name: 'reload', description: 'Force reloads the slash command(s).' }
+};
